a376f7fd753ea7560165af4491fd81fe9c53dafa
Ahmed Elaraby <63804491+DR-3araby@users.noreply.github.com>
2021-07-27 16:15:06 +0200

    Delete personal_website directory

--- personal_website/main/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-main = Blueprint('main', __name__)
-
-from .ar import views
-from .en import views
-from . import errors
-from . import posts
--- personal_website/static/js/clean-blog.js
+++
@@ -1,5 +0,0 @@
-// open external links in new tab
-// https://stackoverflow.com/a/4425214
-$(document.links).filter(function() {
-  return this.hostname != window.location.hostname;
-}).attr('target', '_blank')
